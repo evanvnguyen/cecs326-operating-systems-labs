5f848eaf61cd9667a10d6096d7427e39a3dc3f82
evanvnguyen <nguyen_evan42@yahoo.com>
2022-10-12 16:25:36 -0700

    init main

---
+++ project2/main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <Stdlib.h>
+#include <pthread.h>
+
+int main(){
+	//  5 pthreads per philosopher
+	pthread_t phi1, phi2, phi3, phi4, phi4, phi5;
+	
+	// create independent threads for each
+	return 0;
+}
+
+void pickup_forks(int phi_num){
+
+}
+
+void return_forks(int phi_num){
+
+}
