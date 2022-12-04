19f2e43492ef2a1bae29aca29716ba46abc2b995
evanvnguyen <nguyen_evan42@yahoo.com>
2022-12-04 00:52:40 -0800

    populating struct works need to create lists

--- project4/schedule_fcfs.c
+++ project4/schedule_fcfs.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <memory.h>
 
 #include "task.h"
 #include "list.h"
@@ -33,38 +34,41 @@ struct node returnLinkedList(struct task); // to be examined
 const char filename[] = "schedule.txt";
 
 int main(){
+
 	returnTaskContents();
 	return 0;
 }
 
 int returnTaskContents(void){
 	FILE *fp = fopen(filename, "r");
-	const char s[2] = ",";
-	char *token;
+	const char s[2] = ", ";
+	char *token = (char *)malloc(sizeof(char)*5); 
 	int i;
 	int j = 0;
 
 	Task tasks[8];
+
 	
 	if (fp != NULL){
 		char line[20];
 		while (fgets(line,sizeof line, fp) != NULL){
+			//printf("%d ", j);
 			for (i = 0; i < 3; i++){
 				if (i == 0){
-					// read task name
+					// read task name and assign it
+					// doesnt work
 					token = strtok(line, s);
-					printf("%d %s ", j, token);
-					tasks[j].name = token;
+					//printf("%s ", tasks[j].name);
 				} else if (i == 1) {
 					// read task priority
 					token = strtok(NULL, s);
-					printf("%d ", atoi(token));
 					tasks[j].priority = atoi(token);
+					//printf("%d ", tasks[j].priority);
 				} else if (i == 2) {
 					// read task burst time
 					token = strtok(NULL, s);
 					tasks[j].burst = atoi(token);
-					printf("%d\n", atoi(token));
+					//printf("%d\n", tasks[j].burst);
 				}
 			}
 			j++;
@@ -73,20 +77,25 @@ int returnTaskContents(void){
 	} else {
 		printf("error\n");
 	}
-
-	printf("Name: %s, Priority: %d, Burst: %d\n",
-		       	tasks[0].name,
-		       	tasks[0].priority,
-		       	tasks[0].burst);
+	// FOR SOME REASON, T8 IS COPIED TO ALL name ATTRIBUTES
+	// Manually renamed the name attribute.
+	// Name only contains the number.
+	// Name == tid
+	for (int k = 0; k < 8; k++){
+		char new = k + '1';
+		tasks[k].name = &new;
+		printf("Name: T%c, Priority: %d, Burst: %d\n",
+		       	*tasks[k].name,
+		       	tasks[k].priority,
+		       	tasks[k].burst);
+	}
 	
-	printf("Name: %s, Priority: %d, Burst: %d\n",
-		       	tasks[7].name,
-		       	tasks[7].priority,
-		       	tasks[7].burst);
+	// call returnLinkedList here and add the tasks based on
+		
 	return 1;
 }	
 
-// returns a node
+/// returns a node
 // traverse it in main
 //struct node returnLinkedList(struct task){
 	// add the tasks to the linked list
